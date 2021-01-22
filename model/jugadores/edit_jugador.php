de2162b7027fc3cfb84a14f13ba2b651042fd0e0
Fernando Emmanuel Martínez <41637885+emmanuel0920@users.noreply.github.com>
2021-01-22 09:51:39 -0600

    Arregla el show info
    Agrega campos en modelo vista y controlador

--- model/jugadores/edit_jugador.php
+++ model/jugadores/edit_jugador.php
@@ -4,24 +4,25 @@ include('../../controller/funciones.php');
 
 $voto = $make_mov = 0;
 
-$nombre = $_POST['nombre'];
+$identificador = $_POST['identificador'];
 $a_paterno = $_POST['a_paterno'];
 $a_materno = $_POST['a_materno'];
+$nombre = $_POST['nombre'];
 $calle = $_POST['calle'];
 $numero = $_POST['numero'];
 $colonia = $_POST['colonia'];
 $cp = $_POST['cp'];
 $telefono = $_POST['telefono'];
 $seccion = $_POST['seccion'];
-$casilla = "0";
+$fecha_nacimiento = $_POST['fecha_nacimiento'];
+$edad_jugador = $_POST['edad_jugador'];
+$movilizador = $_POST['movilizador'];
 $id_seccional = $_POST['id_seccional'];
 $id_zonal = $_POST['id_zonal'];
-$posibilidad = "90";
-$a_quien = "2";
-$edad_jugador = $_POST['edad_jugador'];
-$fecha_nacimiento = $_POST['fecha_nacimiento'];
-$movilizador = $_POST['movilizador'];
+$a_quien = $_POST['a_quien'];
+$posibilidad = $_POST['porcen'];
 $observaciones = $_POST['observaciones'];
+$casilla = "0";
 $id = $_POST['id'];
 
 if (isset($_POST['make_mov'])) {
@@ -32,7 +33,7 @@ if (isset($_POST['voto'])) {
 	$voto = 1;
 }
 
-	if(update_jugador($id, $nombre, $a_paterno, $a_materno, $calle, $numero, $colonia, $cp, $telefono, $seccion, $casilla, $id_seccional, $id_zonal, $voto, $posibilidad, $a_quien, $edad_jugador, $fecha_nacimiento, $movilizador, $observaciones))
+	if(update_jugador($id, $nombre, $a_paterno, $a_materno, $calle, $numero, $colonia, $cp, $telefono, $seccion, $casilla, $id_seccional, $id_zonal, $voto, $posibilidad, $a_quien, $edad_jugador, $fecha_nacimiento, $movilizador, $observaciones, $identificador))
 	{
 	    $mensaje = "correcto";
 	}else
